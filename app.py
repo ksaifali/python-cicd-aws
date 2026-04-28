82c7bdfbb1655432bbe94d5cccdb09af2bf66d62
Polar <ksaifali954@gmail.com>
2026-04-28 22:41:06 +0530

    new feature

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello from Cloud Run!", 200
+    return "Hello World!\nWelcome to Flask!", 200
+    return "Hello World!", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
