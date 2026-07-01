f2af65a234093cbd8de66c27a09b9896cfbb7e30
Saif ali khan <ksaifali954@gmail.com>
2026-07-01 14:17:12 +0530

    test: varify-party major bump

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello World!\nWelcome to Flask!", 200
-<<<<<<< Updated upstream
-    return "Hello World!" "yo-yo-honey", 200
-=======
-    return "Hello World!" "yo-yo-honey-singh-yo", 200
->>>>>>> Stashed changes
+    return "Hello World!" "yo-yo-honey-singh-yo-yo", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-test minor bump v2 # or make any real small change
