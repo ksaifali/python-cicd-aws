836d74aeed6e4358f5cb950662e5520023c4cdc1
ksaifali <161710775+ksaifali@users.noreply.github.com>
2026-07-01 14:01:05 +0530

    test: another major bump (#10)

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello World!\nWelcome to Flask!", 200
+<<<<<<< Updated upstream
     return "Hello World!" "yo-yo-honey", 200
+=======
+    return "Hello World!" "yo-yo-honey-singh-yo", 200
+>>>>>>> Stashed changes
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
